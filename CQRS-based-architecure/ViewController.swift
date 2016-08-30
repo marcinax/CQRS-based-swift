dec161cbd90feadb790eaf5374de2e747872512f
Whalla Labs <WhallaLabs@MacBook-Pro-Whalla.local>
2016-08-30 14:21:33 +0200

    init

---
+++ CQRS-based-architecure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CQRS-based-architecure
+//
+//  Created by Whalla Labs on 30.08.2016.
+//  Copyright © 2016 Whalla Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
